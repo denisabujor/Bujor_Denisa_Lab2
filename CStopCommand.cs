8fbba26100b6c2f3e8bdfb5e7243015f4d16f88b
DENISA BUJOR <denisa.bujor@stud.ubbcluj.ro>
2021-10-31 21:42:29 +0200

    Lab 3

---
+++ CStopCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Bujor_Denisa_Lab2.CustomCommands
+{
+   class CStopCommand
+{
+    private static RoutedUICommand launch_command;
+    static CStopCommand()
+    {
+        InputGestureCollection myInputGestures = new InputGestureCollection();
+        myInputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control));
+        launch_command = new RoutedUICommand("Launch", "Launch",
+       typeof(CStopCommand), myInputGestures);
+    }
+    public static RoutedUICommand Launch
+    {
+        get
+        {
+            return launch_command;
+        }
+    }
+}
+}
+
---
+++ Class2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bujor_Denisa_Lab2
+{
+    class Class2
+    {
+    }
+}
